bdeed6e982a382bbcfa39f547b1e287f7e4e45ae
Ole Krüger <okrueger@cognesys.de>
2016-02-10 13:42:18 +0100

    Init from onyx-plugin template

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject onyx-rethinkdb "0.8.2.0-SNAPSHOT"
+  :description "Onyx plugin for rethinkdb"
+  :url "FIX ME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.2.374"]
+                 [org.onyxplatform/onyx "0.8.9"]
+                 [com.apa512/rethinkdb "0.11.0"]]
+  :profiles {:dev {:dependencies []
+                   :plugins []}})
